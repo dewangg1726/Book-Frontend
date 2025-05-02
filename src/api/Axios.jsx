4c55ef1133786f2742c44dd567bf6fc6c62e4a45
dewangg1726 <iamdewang@gmail.com>
2025-05-02 18:03:56 +0530

    Initial commit

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL:'http://localhost:5000/api',
+  baseURL:process.env.REACT_APP_BASE_URL,
   timeout:10_000
 });
 api.interceptors.request.use((cfg) => {
